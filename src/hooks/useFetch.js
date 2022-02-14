359cc8930d43e1575f650bd95c05b5ff0af0e3c8
arif <abcvpc@gmail.com>
2022-02-15 00:52:31 +0600

    try catch error

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,14 +2,31 @@ import { useEffect, useState } from "react";
 
 export const useFetch = (url) => {
   const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(url);
-      const json = await response.json();
-      setData(json);
+      setIsPending(true);
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const json = await response.json();
+
+        setIsPending(false);
+        setData(json);
+      } catch (err) {
+        setIsPending(false);
+        setError("could not fetch data");
+        console.log(err.message);
+      }
     };
     fetchData();
   }, [url]);
-  return { data };
+  console.log(isPending, "hi");
+  return { data, isPending, error };
+  // dont forget to destructure the return values in triplist
 };
